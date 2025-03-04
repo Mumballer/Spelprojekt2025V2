cc13c8b2403da8fd78a0eb68184d695f3176e350
georgelopez <victor.arve@elev.ga.lbs.se>
2025-03-04 18:16:56 +0100

    quest + dialog fully functional

--- Assets/Scripts/QuestUI.cs
+++ Assets/Scripts/QuestUI.cs
@@ -9,7 +9,6 @@ public class QuestUI : MonoBehaviour
     [SerializeField] private GameObject questLogPanel;
     [SerializeField] private Transform questListContainer;
     [SerializeField] private GameObject questEntryPrefab;
-    [SerializeField] private KeyCode toggleKey = KeyCode.J;
     [SerializeField] private TextMeshProUGUI questLogHeaderText;
     [SerializeField] private GameObject noQuestsMessage;
 
@@ -20,20 +19,50 @@ public class QuestUI : MonoBehaviour
     [SerializeField] private Color unselectedTabColor = new Color(0.7f, 0.7f, 0.7f);
 
     [Header("Color Settings")]
-    [SerializeField] private Color inProgressColor = new Color(1f, 0.92f, 0.016f); 
-    [SerializeField] private Color completedColor = new Color(0f, 0.75f, 0.22f); 
+    [SerializeField] private Color inProgressColor = new Color(1f, 0.92f, 0.016f);
+    [SerializeField] private Color completedColor = new Color(0f, 0.75f, 0.22f);
     [SerializeField] private Color questTitleColor = Color.white;
-    [SerializeField] private Color questCompletedTitleColor = new Color(0.5f, 0.5f, 0.5f); 
+    [SerializeField] private Color questCompletedTitleColor = new Color(0.5f, 0.5f, 0.5f);
 
     private List<GameObject> questEntries = new List<GameObject>();
     private bool showingActiveQuests = true;
-    private PlayerController playerController;
+
+    // Store original prefab properties
+    private Vector3 originalPosition;
+    private Vector3 originalScale;
+    private Vector2 originalSizeDelta;
+    private Vector2 originalAnchorMin;
+    private Vector2 originalAnchorMax;
+    private Vector2 originalPivot;
+
+    private void Awake()
+    {
+        // Store the original prefab transform properties
+        if (questEntryPrefab != null)
+        {
+            // Store original transform properties
+            RectTransform prefabRect = questEntryPrefab.GetComponent<RectTransform>();
+            if (prefabRect != null)
+            {
+                originalPosition = prefabRect.localPosition;
+                originalScale = prefabRect.localScale;
+                originalSizeDelta = prefabRect.sizeDelta;
+                originalAnchorMin = prefabRect.anchorMin;
+                originalAnchorMax = prefabRect.anchorMax;
+                originalPivot = prefabRect.pivot;
+            }
+
+            // Make sure the prefab is hidden
+            questEntryPrefab.SetActive(false);
+        }
+    }
 
     private void Start()
     {
+        // Ensure quest log is active from the start
         if (questLogPanel != null)
         {
-            questLogPanel.SetActive(false);
+            questLogPanel.SetActive(true);
         }
 
         if (QuestManager.Instance != null)
@@ -42,8 +71,6 @@ public class QuestUI : MonoBehaviour
             QuestManager.Instance.OnQuestCompleted += RefreshQuestList;
             QuestManager.Instance.OnObjectiveCompleted += (quest, index) => RefreshQuestList(quest);
         }
-
-        playerController = FindFirstObjectByType<PlayerController>();
 
         // Setup tabs
         if (activeQuestsTab != null)
@@ -65,14 +92,7 @@ public class QuestUI : MonoBehaviour
         }
 
         UpdateTabVisuals();
-    }
-
-    private void Update()
-    {
-        if (Input.GetKeyDown(toggleKey))
-        {
-            ToggleQuestLog();
-        }
+        RefreshQuestList(); // Refresh initially
     }
 
     private void OnDestroy()
@@ -82,32 +102,6 @@ public class QuestUI : MonoBehaviour
             QuestManager.Instance.OnQuestAdded -= RefreshQuestList;
             QuestManager.Instance.OnQuestCompleted -= RefreshQuestList;
             QuestManager.Instance.OnObjectiveCompleted -= (quest, index) => RefreshQuestList(quest);
-        }
-    }
-
-    public void ToggleQuestLog()
-    {
-        if (questLogPanel != null)
-        {
-            bool newState = !questLogPanel.activeSelf;
-            questLogPanel.SetActive(newState);
-
-            if (playerController != null)
-            {
-                playerController.SetCanMove(!newState);
-            }
-
-            if (newState)
-            {
-                Cursor.lockState = CursorLockMode.None;
-                Cursor.visible = true;
-                RefreshQuestList();
-            }
-            else
-            {
-                Cursor.lockState = CursorLockMode.Locked;
-                Cursor.visible = false;
-            }
         }
     }
 
@@ -169,11 +163,39 @@ public class QuestUI : MonoBehaviour
             noQuestsMessage.SetActive(questsToShow.Count == 0);
         }
 
-        foreach (var currentQuest in questsToShow)
+        // Disable any layout groups temporarily
+        LayoutGroup[] layoutGroups = questListContainer.GetComponents<LayoutGroup>();
+        foreach (var layout in layoutGroups)
         {
-            GameObject entryObj = Instantiate(questEntryPrefab, questListContainer);
-            QuestEntryUI entryUI = entryObj.GetComponent<QuestEntryUI>();
+            layout.enabled = false;
+        }
 
+        // If we have quests to show, hide the original prefab and show clones
+        for (int i = 0; i < questsToShow.Count; i++)
+        {
+            var currentQuest = questsToShow[i];
+
+            // Instantiate DIRECTLY in the same parent as the prefab
+            GameObject entryObj = Instantiate(questEntryPrefab, questEntryPrefab.transform.parent);
+
+            // Force exact same position and properties in world space
+            RectTransform entryRect = entryObj.GetComponent<RectTransform>();
+            if (entryRect != null)
+            {
+                // Set exact same position as original prefab
+                entryRect.position = questEntryPrefab.GetComponent<RectTransform>().position;
+                entryRect.localScale = originalScale;
+                entryRect.sizeDelta = originalSizeDelta;
+                entryRect.anchorMin = originalAnchorMin;
+                entryRect.anchorMax = originalAnchorMax;
+                entryRect.pivot = originalPivot;
+            }
+
+            // Make it visible
+            entryObj.SetActive(true);
+
+            // Setup the quest data
+            QuestEntryUI entryUI = entryObj.GetComponent<QuestEntryUI>();
             if (entryUI != null)
             {
                 entryUI.SetupQuest(
@@ -192,8 +214,13 @@ public class QuestUI : MonoBehaviour
 
             questEntries.Add(entryObj);
         }
-    }
 
+        // Re-enable layout groups
+        foreach (var layout in layoutGroups)
+        {
+            layout.enabled = true;
+        }
+    }
     private void SetupQuestEntryManually(GameObject entryObj, Quest currentQuest)
     {
         TextMeshProUGUI questNameText = entryObj.transform.Find("QuestTitleText")?.GetComponent<TextMeshProUGUI>();
@@ -206,6 +233,7 @@ public class QuestUI : MonoBehaviour
         {
             Debug.LogWarning("QuestTitleText not found on quest entry prefab");
         }
+
         TextMeshProUGUI descriptionText = entryObj.transform.Find("QuestDescriptionText")?.GetComponent<TextMeshProUGUI>();
         if (descriptionText != null)
         {
@@ -215,6 +243,7 @@ public class QuestUI : MonoBehaviour
         {
             Debug.LogWarning("QuestDescriptionText not found on quest entry prefab");
         }
+
         TextMeshProUGUI objectivesText = entryObj.transform.Find("QuestObjectivesText")?.GetComponent<TextMeshProUGUI>();
         if (objectivesText != null && currentQuest.Objectives != null)
         {
