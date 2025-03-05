747bc72c08aca1524e014ef6553658c1f1a82ec5
localbaldy <filip.aadde@elev.ga.lbs.se>
2025-03-05 10:27:34 +0100

    aidpwa

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+
+    public void PlayGame()
+    {
+        //lodscene
+    }
+
+    public void Options()
+    {
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+
+}
