7f19c0819ebaef44a933dea4618dbdd35e9a667d
localbaldy <filip.aadde@elev.ga.lbs.se>
2025-03-07 13:30:11 +0100

    Scene

--- Assets/SceneChangerDoor.cs
+++ Assets/SceneChangerDoor.cs
@@ -3,14 +3,14 @@ using UnityEngine.SceneManagement;
 
 public class SceneChangerDoor : MonoBehaviour
 {
-
+    public string Scene;
     private void OnTriggerEnter(Collider other)
     {
         // Kolla om det är spelaren som kolliderar
         if (other.gameObject.CompareTag("Player"))
         {
             // Byt scen, exempelvis till scenen med index 1
-            SceneManager.LoadScene("FinalScene");  // Byt till den scen du vill
+            SceneManager.LoadScene(Scene);  // Byt till den scen du vill
         }
 
     }
